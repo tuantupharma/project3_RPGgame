6e33c50ffeb09a7f960f5a3db5535dc6c9dd5ce4
Hoang Tuan Tu <hoangtuantu@outlook.com>
2022-03-16 14:10:28 +0700

    vid 3. add melee weapon script to swords

---
+++ Assets/RpgAventure/Scripts/Weapons/MeleeWeapon.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace RpgAdventure
+{
+
+public class MeleeWeapon : MonoBehaviour
+{
+        public int damage = 10;
+
+
+
+
+}
+}
