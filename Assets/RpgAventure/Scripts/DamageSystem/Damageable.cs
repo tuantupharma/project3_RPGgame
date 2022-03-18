565997e287fd27e9e62e1666bf844e0c21a4907e
Hoang Tuan Tu <hoangtuantu@outlook.com>
2022-03-18 10:02:24 +0700

    vid11. Draw hit Angle, add Range slider for hitAngle

--- Assets/RpgAventure/Scripts/DamageSystem/Damageable.cs
+++ Assets/RpgAventure/Scripts/DamageSystem/Damageable.cs
@@ -7,11 +7,33 @@ namespace RpgAdventure
     public partial class Damageable : MonoBehaviour
     {
         public int maxHitPoints;
+        [Range(0,360f)]
+        public float hitAngle = 360f;
 
         public void ApplyDamage()
         {
             //Debug.Log("Applying dmg");
         }
+#if UNITY_EDITOR
+        private void OnDrawGizmosSelected()
+        {
+            UnityEditor.Handles.color = new Color(0.0f,0.0f,1.0f,0.5f);
+
+            Vector3 rotatedForward = 
+                Quaternion.AngleAxis(
+                -hitAngle * 0.5f, transform.up) * 
+                transform.forward;
+
+            UnityEditor.Handles.DrawSolidArc(
+                transform.position,
+                transform.up,
+                rotatedForward,
+                hitAngle,
+                1.0f );
+        }
+
+
+#endif
     }
 }
 
