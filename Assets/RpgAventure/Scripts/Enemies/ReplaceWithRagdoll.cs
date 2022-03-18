bfb57e131cff2c0d348f5abe9c39763b89488c89
Hoang Tuan Tu <hoangtuantu@outlook.com>
2022-03-18 14:26:19 +0700

    vid3. Replace with Ragdoll start

---
+++ Assets/RpgAventure/Scripts/Enemies/ReplaceWithRagdoll.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace RpgAdventure
+{
+    public class ReplaceWithRagdoll : MonoBehaviour
+    {
+        public void Replace()
+        {
+            Debug.Log("Replacing with the ragdoll");
+        }
+
+
+    }
+}
