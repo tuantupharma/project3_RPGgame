d705b6694c2d2090bb82aa872956abbccd792314
Hoang Tuan Tu <hoangtuantu@outlook.com>
2022-03-19 09:11:19 +0700

    vid4. Quest and quest manager

---
+++ Assets/RpgAventure/Scripts/Quest/Quest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace RpgAdventure
+{
+    public enum QuestType
+    {
+        HUNT,
+        GATHER,
+        TALK,
+        EXPLORE
+    }
+
+    [System.Serializable]
+    public class Quest
+    {
+
+        public string uid;
+        public string title;
+        public string description;
+        public string experience;
+        public int gold;
+
+        public int amount;
+        public string[] targets;
+
+        public string talkTo;
+        public Vector3 explore;
+
+        public string questOwner;
+        public QuestType type;
+    }
+
+}
---
+++ Assets/RpgAventure/Scripts/Quest/QuestManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RpgAdventure
+{
+    public class QuestManager : MonoBehaviour
+    {
+        public List<Quest> quests;
+
+
+
+
+    }
+
+
+}
